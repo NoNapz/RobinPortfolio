f95b323cbe760198dd295104be19b8cb6eab71c5
Robin Hellström <robin1robin@hotmail.com>
2021-12-15 09:32:12 +0100

    Add project files.

---
+++ Client/Services/InMemoryDBC.cs
@@ -0,0 +1,40 @@
+﻿using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services
+{
+    internal sealed class InMemoryDBC
+    {
+        private readonly HttpClient _httpClient;
+
+        public InMemoryDBC(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private string GitAPIUri = "https://api.github.com/users/NoNapz/repos";
+        private List<GithubRepos> _githubRepos = null;
+
+        internal List<GithubRepos> GitRepos
+        {
+            get
+            {
+                return _githubRepos;
+            }
+            set
+            {
+                _githubRepos = value;
+                NotifyGitRepoDataChanged();
+            }
+        }
+
+        internal async Task GetGithubReposFromCache()
+        {
+            _githubRepos = await _httpClient.GetFromJsonAsync<List<GithubRepos>>(GitAPIUri);
+        }
+
+        internal event Action OnGitRepoDataChange;
+        private void NotifyGitRepoDataChanged() => OnGitRepoDataChange?.Invoke();
+        
+    }
+}
---
+++ Shared/Models/GithubRepos.cs
@@ -0,0 +1,20 @@
+﻿namespace Shared.Models
+{
+    public class GithubRepos
+    {
+        public string Name { get; set; }
+        public string Full_Name { get; set; }
+        public Owner Owner { get; set; }
+        public string Html_Url { get; set; }
+        public object Description { get; set; }
+        public DateTime Created_At { get; set; }
+        public DateTime Updated_At { get; set; }
+        public string Language { get; set; }
+        public string Visibility { get; set; }
+    }
+    public class Owner
+    {
+        public string Login { get; set; }
+        public string Avatar_Url { get; set; }
+    }
+}
